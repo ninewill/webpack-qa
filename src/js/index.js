910ed8a8884134e7a2f7a479ed8febd2f56f23c3
will <wtony1239271@gmail.com>
2019-03-29 10:21:34 +0800

    webpqck

---
+++ src/js/index.js
@@ -0,0 +1,88 @@
+import "reset.scss";
+import "index.scss";
+
+// import Item from "Item.js"
+// console.log(Item);
+
+// console.log($(".header-bg"))
+
+
+
+// var tmax_optionsGlobal = {
+//     repeat: 0,
+//     repeatDelay: 2,
+//     yoyo: false
+// };
+
+// CSSPlugin.useSVGTransformAttr = true;
+
+// var tl = new TimelineMax(tmax_optionsGlobal),
+//     path = 'svg *',
+//     stagger_val = .1,
+//     duration = .5;
+
+// $.each($(path), function (i, el) {
+//     tl.set($(this), {
+//         x: '+=' + getRandom(0, 0),
+//         y: '+=' + getRandom(0, 0),
+//         rotation: '+=' + getRandom(0, 0),
+//         scale: 0,
+//         opacity: 0
+//     });
+// });
+
+// var stagger_opts_to = {
+//     x: 0,
+//     y: 0,
+//     opacity: 1,
+//     scale: 1,
+//     rotation: 0,
+//     ease: SlowMo.ease.config(.2, .2, false),
+
+
+
+// };
+
+// tl.staggerTo(path, duration, stagger_opts_to, stagger_val);
+
+// var $svg = $('svg');
+// $svg.hover(
+//     function () {
+//         tl.timeScale(0.15);
+//     },
+//     function () {
+//         tl.timeScale(1);
+//     });
+
+// function getRandom(min, max) {
+//     return Math.random() * (max - min) + min;
+// }
+
+
+
+// var roundLogEl = document.querySelector('.round-log');
+
+// anime({
+//     targets: roundLogEl,
+//     innerHTML: [0, 100],
+//     easing: 'linear',
+//     round: 10
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+var path = require("path");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var extractCSS = new ExtractTextPlugin('css/[name].css');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	context: path.resolve(__dirname, "./src"),
+	entry: {
+		index: 'index',
+		about: 'about',
+	},
+	output: {
+		path: path.resolve(__dirname, "./dist"),
+		filename: './js/[name].js?[hash:8]',
+	},
+	resolve: {
+		modules: [
+			path.resolve('src'),
+			path.resolve('src/js'),
+			path.resolve('src/js/object'),
+			path.resolve('src/scss'),
+			path.resolve('src/images'),
+			path.resolve('src/assets'),
+			path.resolve('node_modules')
+		],
+		extensions: ['.js']
+	},
+	devServer: {
+		compress: true,
+		port: 3000,
+		stats: {
+			assets: true,
+			cached: false,
+			chunkModules: false,
+			chunkOrigins: false,
+			chunks: false,
+			colors: true,
+			hash: false,
+			modules: false,
+			reasons: false,
+			source: false,
+			version: false,
+			warnings: false
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(woff|woff2|ttf|eot)$/,
+				loader: 'file-loader',
+				options: {
+					name: '[path][name].[ext]?[hash:8]'
+				}
+			},
+			{
+				test: /\.css$/,
+				use: extractCSS.extract(['css-loader', 'postcss-loader'])
+			},
+			{
+				test: /\.(sass|scss)$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'postcss-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.(js)$/,
+				use: 'babel-loader'
+			},
+			{
+				test: /\.(jpe?g|png|gif)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+							name: '[path][name].[ext]?[hash:8]'
+						}
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								progressive: true,
+								quality: 65
+							},
+							optipng: {
+								enabled: false,
+							},
+							pngquant: {
+								quality: '65-90',
+								speed: 4
+							},
+							gifsicle: {
+								interlaced: false,
+							},
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		extractCSS,
+		new CopyWebpackPlugin([
+			{ from: 'assets', to: 'assets' }
+		]),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery'
+		}),
+		new HtmlWebpackPlugin({
+			title: 'metro',
+			filename: 'index.html',
+			template: 'html/index.html',
+			viewport: 'width=device-width, initial-scale=1.0',
+			chunks: ['index'],
+		}),
+		new HtmlWebpackPlugin({
+			title: 'about',
+			filename: 'about.html',
+			template: 'html/about.html',
+			viewport: 'width=device-width, initial-scale=1.0',
+			chunks: ['about'],
+		}),
+	]
+}
